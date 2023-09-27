19d43f9cb405d442b45b0b0b9e4e1c2cae2645f1
Lường Đại Phát <21522443@gm.uit.edu.vn>
2023-09-27 09:16:41 +0700

    Phat ne

---
+++ Desktop/Phat/Test.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloWorld
+{
+    public class HelloWorld
+    {
+        internal void Main(string[] args)
+        {
+            Console.WriteLine("Phat ne ae");
+        }
+    }
+}
