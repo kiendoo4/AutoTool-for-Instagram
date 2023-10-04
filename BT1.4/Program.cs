e19b780272a07b7a8c75a605fbcb073110e921b5
AnTran210 <21520553@gm.uit.edu.vn>
2023-10-04 15:15:08 +0700

    add bt1.4

---
+++ BT1.4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BT1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
