31d47aab760198cbc301e171b2f683248dd00dc4
Lường Đại Phát <21522443@gm.uit.edu.vn>
2023-10-05 13:07:29 +0700

    ADD BT1.5

---
+++ Lab/Lab1/BT1.5/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.IO;
+
+namespace BT1._5
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the directory path:");
+            string directoryPath = Console.ReadLine();
+
+            if (Directory.Exists(directoryPath))
+            {
+                string[] files = Directory.GetFiles(directoryPath);
+
+                if (files.Length > 0)
+                {
+                    Console.WriteLine("Files in the directory:");
+                    foreach (string file in files)
+                    {
+                        Console.WriteLine(Path.GetFileName(file));
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No files found in the directory.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("The specified directory does not exist.");
+            }
+        }
+    }
+}
