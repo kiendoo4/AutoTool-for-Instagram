e197c3a46e9cac85604d034e1ba005b2f24b8a19
Lường Đại Phát <93922753+luongdaiphat@users.noreply.github.com>
2023-10-28 23:08:05 +0700

    Add bt2.5

---
+++ Lab/Lab2/BT2.5/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tvwDirs = new System.Windows.Forms.TreeView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.renameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pasteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.largeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.smallToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.detailsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cbxAddress = new System.Windows.Forms.ComboBox();
+            this.lvwExplorer = new System.Windows.Forms.ListView();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.tvwDirs);
+            this.splitContainer1.Panel1.Controls.Add(this.menuStrip1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.cbxAddress);
+            this.splitContainer1.Panel2.Controls.Add(this.lvwExplorer);
+            this.splitContainer1.Size = new System.Drawing.Size(1175, 602);
+            this.splitContainer1.SplitterDistance = 391;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // tvwDirs
+            // 
+            this.tvwDirs.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tvwDirs.Location = new System.Drawing.Point(0, 24);
+            this.tvwDirs.Name = "tvwDirs";
+            this.tvwDirs.Size = new System.Drawing.Size(391, 578);
+            this.tvwDirs.TabIndex = 0;
+            this.tvwDirs.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.tvwDirs_AfterSelect);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.viewsToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(391, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.renameToolStripMenuItem,
+            this.deleteToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // renameToolStripMenuItem
+            // 
+            this.renameToolStripMenuItem.Name = "renameToolStripMenuItem";
+            this.renameToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.renameToolStripMenuItem.Text = "Rename";
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(114, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyToolStripMenuItem,
+            this.cutToolStripMenuItem,
+            this.pasteToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.copyToolStripMenuItem.Text = "Copy";
+            this.copyToolStripMenuItem.Click += new System.EventHandler(this.copyToolStripMenuItem_Click);
+            // 
+            // cutToolStripMenuItem
+            // 
+            this.cutToolStripMenuItem.Name = "cutToolStripMenuItem";
+            this.cutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.cutToolStripMenuItem.Text = "Cut";
+            this.cutToolStripMenuItem.Click += new System.EventHandler(this.cutToolStripMenuItem_Click);
+            // 
+            // pasteToolStripMenuItem
+            // 
+            this.pasteToolStripMenuItem.Name = "pasteToolStripMenuItem";
+            this.pasteToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.pasteToolStripMenuItem.Text = "Paste";
+            this.pasteToolStripMenuItem.Click += new System.EventHandler(this.pasteToolStripMenuItem_Click);
+            // 
+            // viewsToolStripMenuItem
+            // 
+            this.viewsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.largeToolStripMenuItem,
+            this.smallToolStripMenuItem,
+            this.detailsToolStripMenuItem,
+            this.listToolStripMenuItem});
+            this.viewsToolStripMenuItem.Name = "viewsToolStripMenuItem";
+            this.viewsToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.viewsToolStripMenuItem.Text = "Views";
+            // 
+            // largeToolStripMenuItem
+            // 
+            this.largeToolStripMenuItem.Name = "largeToolStripMenuItem";
+            this.largeToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.largeToolStripMenuItem.Text = "Large";
+            this.largeToolStripMenuItem.Click += new System.EventHandler(this.largeToolStripMenuItem_Click);
+            // 
+            // smallToolStripMenuItem
+            // 
+            this.smallToolStripMenuItem.Name = "smallToolStripMenuItem";
+            this.smallToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.smallToolStripMenuItem.Text = "Small";
+            this.smallToolStripMenuItem.Click += new System.EventHandler(this.smallToolStripMenuItem_Click);
+            // 
+            // detailsToolStripMenuItem
+            // 
+            this.detailsToolStripMenuItem.Name = "detailsToolStripMenuItem";
+            this.detailsToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.detailsToolStripMenuItem.Text = "Details";
+            this.detailsToolStripMenuItem.Click += new System.EventHandler(this.detailsToolStripMenuItem_Click);
+            // 
+            // listToolStripMenuItem
+            // 
+            this.listToolStripMenuItem.Name = "listToolStripMenuItem";
+            this.listToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.listToolStripMenuItem.Text = "List";
+            this.listToolStripMenuItem.Click += new System.EventHandler(this.listToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // cbxAddress
+            // 
+            this.cbxAddress.FormattingEnabled = true;
+            this.cbxAddress.Location = new System.Drawing.Point(212, 13);
+            this.cbxAddress.Name = "cbxAddress";
+            this.cbxAddress.Size = new System.Drawing.Size(383, 21);
+            this.cbxAddress.TabIndex = 1;
+            this.cbxAddress.KeyDown += new System.Windows.Forms.KeyEventHandler(this.cbxAddress_KeyDown);
+            // 
+            // lvwExplorer
+            // 
+            this.lvwExplorer.HideSelection = false;
+            this.lvwExplorer.Location = new System.Drawing.Point(3, 46);
+            this.lvwExplorer.Name = "lvwExplorer";
+            this.lvwExplorer.Size = new System.Drawing.Size(777, 556);
+            this.lvwExplorer.TabIndex = 0;
+            this.lvwExplorer.UseCompatibleStateImageBehavior = false;
+            this.lvwExplorer.DoubleClick += new System.EventHandler(this.lvwExplorer_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1175, 602);
+            this.Controls.Add(this.splitContainer1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView tvwDirs;
+        private System.Windows.Forms.ListView lvwExplorer;
+        private System.Windows.Forms.ComboBox cbxAddress;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem renameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pasteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem largeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem smallToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem detailsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listToolStripMenuItem;
+    }
+}
+
---
+++ Lab/Lab2/BT2.5/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ids = Directory.GetLogicalDrives();
+            foreach (string id in ids) { 
+                tvwDirs.Nodes.Add(id);
+                lvwExplorer.Items.Add(id, 8);
+                    
+            }
+        }
+        private void assignIcons(string[] file)
+        {
+            foreach (string fl in file)
+            {
+                if (fl.EndsWith(".JPG") || fl.EndsWith(".jpg") || fl.EndsWith(".png") || fl.EndsWith(".PNG") || fl.EndsWith(".GIF") || fl.EndsWith(".gif"))
+                    lvwExplorer.Items.Add(fl, 2);
+                else if (fl.EndsWith(".MP4") || fl.EndsWith(".mp4") || fl.EndsWith(".avi") || fl.EndsWith(".AVI") || fl.EndsWith(".MPEG") || fl.EndsWith(".mpeg"))
+                    lvwExplorer.Items.Add(fl, 3);
+                else if (fl.EndsWith(".txt") || fl.EndsWith(".TXT"))
+                    lvwExplorer.Items.Add(fl, 4);
+                else if (fl.EndsWith(".doc") || fl.EndsWith(".DOC") || fl.EndsWith(".docx") || fl.EndsWith(".DOCX"))
+                    lvwExplorer.Items.Add(fl, 5);
+                else if (fl.EndsWith(".exe") || fl.EndsWith(".EXE"))
+                    lvwExplorer.Items.Add(fl, 6);
+                else if (fl.EndsWith(".msi") || fl.EndsWith(".MSI"))
+                    lvwExplorer.Items.Add(fl, 7);
+                else if (fl.EndsWith(".dll") || fl.EndsWith(".DLL"))
+                    lvwExplorer.Items.Add(fl, 9);
+                else if (fl.EndsWith(".lnk") || fl.EndsWith(".LNK"))
+                    lvwExplorer.Items.Add(fl, 10);
+                else
+                    lvwExplorer.Items.Add(fl, 1);
+            }
+        }
+        private void showDriveDirects()
+        {
+            try
+            {
+                lvwExplorer.Clear();
+                string[] dirs = Directory.GetDirectories(tvwDirs.SelectedNode.Text);
+                string[] files = Directory.GetFiles(tvwDirs.SelectedNode.Text);
+                foreach ( string dir in dirs )
+                {
+                    lvwExplorer.Items.Add(dir, 0);
+                    tvwDirs.SelectedNode.Nodes.Add(dir);
+                }
+                assignIcons(files);
+                cbxAddress.Text=tvwDirs.SelectedNode.Text;
+                
+            }
+            catch (Exception ex){
+                MessageBox.Show("Error - " + ex.Message);
+            
+            }
+        }
+        private void GotoDirectory()
+        {
+            try
+            {
+                lvwExplorer.Clear();
+                string[] dirs = Directory.GetDirectories(cbxAddress.Text);
+                string[] files = Directory.GetFiles(cbxAddress.Text);
+                foreach (string dir in dirs)
+                {
+                    lvwExplorer.Items.Add(dir, 0);
+                }
+                assignIcons(files);
+                if (cbxAddress.Items.Contains(cbxAddress.Text) == false)
+                    cbxAddress.Items.Add(cbxAddress.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Fehler -" + ex.Message);
+            }
+        }
+
+        private void largeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            lvwExplorer.View = View.LargeIcon;
+        }
+
+        private void smallToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            lvwExplorer.View = View.SmallIcon;
+        }
+
+        private void detailsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            lvwExplorer.View= View.Details;
+        }
+
+        private void listToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            lvwExplorer.View = View.List;
+        }
+
+
+        private void lvwExplorer_DoubleClick(object sender, EventArgs e)
+        {
+            string direc = lvwExplorer.SelectedItems[0].Text;
+            try
+            {
+                lvwExplorer.Clear();
+                string[] dirs = Directory.GetDirectories(direc);
+                string[] files = Directory.GetFiles(direc);
+                foreach (string dir in dirs)
+                {
+                    lvwExplorer.Items.Add(dir, 0);
+                }
+                assignIcons(files);
+                cbxAddress.Text = direc;
+                if (cbxAddress.Items.Contains(cbxAddress.Text) == false)
+                    cbxAddress.Items.Add(direc);
+            }
+            catch (Exception ex)
+            {
+                try
+                {
+                    cbxAddress.Text = direc;
+                    System.Diagnostics.Process.Start(direc);
+                }
+                catch(Exception ex2)
+                {
+                    MessageBox.Show("Fehler -" + ex.Message);
+                }
+            }
+        }
+
+        private void cbxAddress_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                GotoDirectory();
+            }
+        }
+
+        private void tvwDirs_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            showDriveDirects();
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (lvwExplorer.SelectedItems[0].Text != "" && lvwExplorer.SelectedItems.Count == 1)
+                Clipboard.SetText(lvwExplorer.SelectedItems[0].Text);
+            else
+                MessageBox.Show("You can only copy one element at a time", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string path = Clipboard.GetText();
+            char seperator = '\\';
+            string originalFilename = path.Split(seperator)[path.Split(seperator).Length - 1];
+            string taget = cbxAddress.Text + '\\' + originalFilename;
+            try
+            {
+                if (File.Exists(taget))
+                    if (MessageBox.Show("Exits, do u want to replace it?", "File exits", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                        File.Delete(taget);
+                File.Copy(path, taget, false);
+                GotoDirectory();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error - " + ex.Message);
+            }
+        }
+
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (lvwExplorer.SelectedItems.Count == 1)
+                {
+                    if (MessageBox.Show("Do u want to delete this file?", "Delete", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                        File.Delete(lvwExplorer.SelectedItems[0].Text);
+                }
+                else
+                    MessageBox.Show("You can only delete one item at a time.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error - " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            GotoDirectory();
+        }
+    }
+}
