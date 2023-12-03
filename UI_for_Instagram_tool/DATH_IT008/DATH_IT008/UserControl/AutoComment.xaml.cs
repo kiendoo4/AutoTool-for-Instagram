4916b375fa81a25acbab5c7ccdfdfd457f1ef8a1
Trung Kien Nguyen <koke36354@gmail.com>
2023-12-03 12:56:53 +0700

    finish autocomment

--- UI_for_Instagram_tool/DATH_IT008/DATH_IT008/UserControl/AutoComment.xaml.cs
+++ UI_for_Instagram_tool/DATH_IT008/DATH_IT008/UserControl/AutoComment.xaml.cs
@@ -26,6 +26,7 @@ namespace DATH_IT008.UserControl
     /// </summary>
     public partial class AutoComment
     {
+        bool check1 = false, check2 = false;
         List<ChromeDriver> chromedrivers = new List<ChromeDriver>(); 
         List<string> commentList = new List<string>();
         List<string> directoryList = new List<string>();
@@ -40,12 +41,73 @@ namespace DATH_IT008.UserControl
         }
         private void FinishClick(object sender, RoutedEventArgs e)
         {
-
+            if(!check1 || !check2)
+            {
+                MessageBox.Show("Vui lòng chọn đầy đủ đường dẫn và comment", "Thông báo");
+            }    
+            else
+            {
+                Random random = new Random();
+                for (int i = 0; i < directoryList.Count; i++)
+                {
+                    foreach (ChromeDriver chromeDriver in chromedrivers)
+                    {
+                        chromeDriver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(20);
+                        chromeDriver.Navigate().GoToUrl(directoryList[i]);
+                        try
+                        {
+                            Thread.Sleep(5000);
+                            //Find element
+                            IList<IWebElement> elements = chromeDriver.FindElements(By.CssSelector("textarea[class='x1i0vuye xvbhtw8 x1ejq31n xd10rxx x1sy0etr x17r0tee x5n08af x78zum5 x1iyjqo2 x1qlqyl8 x1d6elog xlk1fp6 x1a2a7pz xexx8yu x4uap5 x18d9i69 xkhd6sd xtt52l0 xnalus7 xs3hnx8 x1bq4at4 xaqnwrm']")).Take(5).ToList();
+                            chromeDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+                            if (elements.Count > 0)
+                            {
+                                foreach (var element in elements)
+                                {
+                                    int randomNumber = random.Next(0, commentList.Count);
+                                    element.SendKeys(commentList[randomNumber]);
+                                    Thread.Sleep(3000);
+                                    IWebElement button = chromeDriver.FindElement(By.CssSelector("div[class='x1i10hfl xjqpnuy xa49m3k xqeqjp1 x2hbi6w xdl72j9 x2lah0s xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r x2lwn1j xeuugli x1hl2dhg xggy1nq x1ja2u2z x1t137rt x1q0g3np x1lku1pv x1a2a7pz x6s0dn4 xjyslct x1ejq31n xd10rxx x1sy0etr x17r0tee x9f619 x1ypdohk x1f6kntn xwhw2v2 xl56j7k x17ydfre x2b8uid xlyipyv x87ps6o x14atkfc xcdnw81 x1i0vuye xjbqb8w xm3z3ea x1x8b98j x131883w x16mih1h x972fbf xcfux6l x1qhh985 xm0m39n xt0psk2 xt7dq6l xexx8yu x4uap5 x18d9i69 xkhd6sd x1n2onr6 x1n5bzlp x173jzuc x1yc6y37']"));
+                                    Thread.Sleep(3000);
+                                    button.Click();
+                                    Thread.Sleep(3000);
+                                }
+                            }
+                        }
+                        finally
+                        {
+                            Thread.Sleep(10000);
+                            chromeDriver.Quit();
+                        }
+                    }
+                }
+            }
         }
 
         private void ChooseCommentList(object sender, RoutedEventArgs e)
         {
-
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Text Files|*.txt|All Files|*.*", // Filter for text files
+                Title = "Chọn file txt"
+            };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string textFilePath = openFileDialog.FileName;
+                string[] fileContent = System.IO.File.ReadAllLines(textFilePath);
+                for (int i = 0; i < fileContent.Length; i++)
+                {
+                    commentList.Add(fileContent[i]);
+                }
+                MessageBox.Show("Đã cập nhật danh sách comment", "Thông báo");
+                check1 = true;
+                if (check1 && check2)
+                    FinishButton.IsEnabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Hủy thực thi", "Thông báo");
+            }
         }
         private void ChooseDirectiryClick(object sender, RoutedEventArgs e)
         {
@@ -58,50 +120,16 @@ namespace DATH_IT008.UserControl
             // Show the dialog and check if the user clicked OK
             if (openFileDialog.ShowDialog() == true)
             {
-                //// Get the selected file path
-                //string textFilePath = openFileDialog.FileName;
-
-                //// Read the content of the text file
-                //string[] fileContent = System.IO.File.ReadAllLines(textFilePath);
-                //for (int i = 0; i < fileContent.Length; i++)
-                //{
-
-                //    foreach (ChromeDriver chromeDriver in chromedrivers)
-                //    {
-                //        chromeDriver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(20);
-                //        try
-                //        {
-                //            Thread.Sleep(5000);
-                //            //Find element
-                //            IList<IWebElement> elements = chromeDriver.FindElements(By.CssSelector("textarea[class='x1i0vuye xvbhtw8 x1ejq31n xd10rxx x1sy0etr x17r0tee x5n08af x78zum5 x1iyjqo2 x1qlqyl8 x1d6elog xlk1fp6 x1a2a7pz xexx8yu x4uap5 x18d9i69 xkhd6sd xtt52l0 xnalus7 xs3hnx8 x1bq4at4 xaqnwrm']")).Take(5).ToList();
-                //            chromeDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
-                //            if (elements.Count > 0)
-                //            {
-                //                foreach (var element in elements)
-                //                {
-                //                    element.SendKeys("beautiful");
-                //                    Thread.Sleep(3000);
-                //                    IWebElement button = chromeDriver.FindElement(By.CssSelector("div[class='x1i10hfl xjqpnuy xa49m3k xqeqjp1 x2hbi6w xdl72j9 x2lah0s xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r x2lwn1j xeuugli x1hl2dhg xggy1nq x1ja2u2z x1t137rt x1q0g3np x1lku1pv x1a2a7pz x6s0dn4 xjyslct x1ejq31n xd10rxx x1sy0etr x17r0tee x9f619 x1ypdohk x1f6kntn xwhw2v2 xl56j7k x17ydfre x2b8uid xlyipyv x87ps6o x14atkfc xcdnw81 x1i0vuye xjbqb8w xm3z3ea x1x8b98j x131883w x16mih1h x972fbf xcfux6l x1qhh985 xm0m39n xt0psk2 xt7dq6l xexx8yu x4uap5 x18d9i69 xkhd6sd x1n2onr6 x1n5bzlp x173jzuc x1yc6y37']"));
-                //                    Thread.Sleep(3000);
-                //                    button.Click();
-                //                    Thread.Sleep(3000);
-                //                }
-                //            }
-                //        }
-                //        finally
-                //        {
-                //            Thread.Sleep(10000);
-                //            chromeDriver.Quit();
-                //        }
-                //    }
-                //}
                 string textFilePath = openFileDialog.FileName;
                 string[] fileContent = System.IO.File.ReadAllLines(textFilePath);
                 for (int i = 0; i < fileContent.Length; i++)
                 {
                     directoryList.Add(fileContent[i]);
                 }
-                MessageBox.Show(directoryList[0]);
+                MessageBox.Show("Đã cập nhật danh sách đường dẫn", "Thông báo");
+                check2 = true;
+                if (check1 && check2)
+                    FinishButton.IsEnabled = true;
             }
             else
             {
