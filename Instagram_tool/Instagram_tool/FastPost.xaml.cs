459abde450f1c23d2f4f1879eb6bd57a8b7e3432
Phạm Việt <74823960+HatakaCder@users.noreply.github.com>
2023-12-03 09:12:46 +0700

    Upload instagram_tool project

---
+++ Instagram_tool/Instagram_tool/FastPost.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Instagram_tool
+{
+    /// <summary>
+    /// Interaction logic for FastPost.xaml
+    /// </summary>
+    public partial class FastPost : Window
+    {
+        /* TO DO LIST
+         * 
+         * - Big to do: Đăng bài + ảnh + caption + option (Ẩn like và views + tắt bình luận + Accessibility)
+         * 
+         * - Features:
+         * + Invidual post + invidual image (video): Đăng bài đơn + caption cụ thể (hoặc random)
+         * + Invidual post + multiple images (videos): Đăng bài đơn nhưng bài đó chứa nhiều ảnh hoặc video + caption cụ thể (hoặc random)
+         * + Multiple posts + invidual image (video): Đăng nhiều bài + caption cụ thể cho từng bài hoặc một caption cho all (hoặc random) 
+         * + Multiple posts + multiple images (videos): như feature 3 nhưng nhiều files
+         */
+
+        public FastPost()
+        {
+            InitializeComponent();
+        }
+        private void rad_invidual_Checked(object sender, RoutedEventArgs e)
+        {
+            frame_FastPost.Navigate(new FastPost_invidual(tbx_path.Text));
+        }
+
+        private void rad_multiple_Checked(object sender, RoutedEventArgs e)
+        {
+            frame_FastPost.Navigate(new FastPost_multiple(tbx_path.Text));
+        }
+
+        private void frame_FastPost_Loaded(object sender, RoutedEventArgs e)
+        {
+            rad_invidual.IsChecked = true;
+        }
+    }
+}
---
+++ Instagram_tool/Instagram_tool/Liketool.xaml.cs
@@ -0,0 +1,82 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Instagram_tool
+{
+    /// <summary>
+    /// Interaction logic for Liketool.xaml
+    /// </summary>
+    public partial class Liketool : Window
+    {
+        Abilities ab = new Abilities();
+        ChromeDriver driver;
+        // for testing
+        private void LoadProfile()
+        {
+            ChromeOptions options = new ChromeOptions();
+            string ProfilePath = "C:\\Users\\Admin\\AppData\\Local\\Google\\Chrome\\User Data\\TestUser"; // change this directory to your chrome profile path if you use this code
+            if (!Directory.Exists(ProfilePath))
+            {
+                Directory.CreateDirectory(ProfilePath);
+            }
+            options.AddArgument("user-data-dir=" + ProfilePath);
+            driver = new ChromeDriver(options);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5); // change max wait time (second) here
+            driver.Manage().Window.Maximize();
+        }
+        public Liketool()
+        {
+            InitializeComponent();
+        }
+
+        private HashSet<string> getPosts()
+        {
+            HashSet<string> posts = new HashSet<string>();
+            if (tbx_posts.Text != "")
+            {
+                posts = new HashSet<string>(tbx_posts.Text.Split("\r\n"));
+            }
+            else
+            {
+                // use dummy posts
+                posts = new HashSet<string>{
+                    "https://www.instagram.com/p/Czs308Pv-Yc/",
+                    "https://www.instagram.com/p/CzeG_YgvSj6",
+                    "https://www.instagram.com/p/CzaR2gQPUxc/"
+                };
+            }
+            return posts;
+        }
+        
+        
+
+        // Like button
+        private void btn_like_Click(object sender, RoutedEventArgs e)
+        {
+            LoadProfile();
+            HashSet<string> posts = getPosts();
+            ab.like_unlike(driver, posts, "Like");
+        }
+
+        // Unlike button
+        private void btn_unlike_Click(object sender, RoutedEventArgs e)
+        {
+            LoadProfile();
+            HashSet<string> posts = getPosts();
+            ab.like_unlike(driver, posts, "Unlike");
+        }
+    }
+}
