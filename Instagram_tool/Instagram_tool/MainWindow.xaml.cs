780d26fb3a1aba4867b42e1b833420e4135412f6
Phạm Việt <74823960+HatakaCder@users.noreply.github.com>
2023-12-04 15:36:32 +0700

    Update 2 features Like/unlike + FastPost

--- Instagram_tool/Instagram_tool/MainWindow.xaml.cs
+++ Instagram_tool/Instagram_tool/MainWindow.xaml.cs
@@ -1,7 +1,13 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,6 +26,20 @@ namespace Instagram_tool
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ChromeDriver driver;
+        private void LoadProfile()
+        {
+            ChromeOptions options = new ChromeOptions();
+            string ProfilePath = "C:\\Users\\Admin\\AppData\\Local\\Google\\Chrome\\User Data\\TestUser"; // change this directory to your chrome profile path if you use this code
+            if (!Directory.Exists(ProfilePath))
+            {
+                Directory.CreateDirectory(ProfilePath);
+            }
+            options.AddArgument("user-data-dir=" + ProfilePath);
+            driver = new ChromeDriver(options);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5); // change max wait time (second) here
+            driver.Manage().Window.Maximize();
+        }
         public MainWindow()
         {
             InitializeComponent();
@@ -35,6 +55,18 @@ namespace Instagram_tool
         {
             FastPost fastPost = new FastPost();
             fastPost.Show();
+        }
+
+        private void btn_test_Click(object sender, RoutedEventArgs e)
+        {
+            LoadProfile();
+            driver.Navigate().GoToUrl("https://www.instagram.com/hatakacder/");
+            var a = driver.FindElement(By.XPath("//button[contains(@class,'_acan _acap _acat _aj1- _ap30')]"));
+            a.Click();
+            Thread.Sleep(3000);
+            a = driver.FindElement(By.XPath("//span[contains(text(),'Unfollow')]"));
+            a.Click();
+
         }
     }
 }
